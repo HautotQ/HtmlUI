e1c66382fc4b3a43fa156920f83297a91c6fbda5
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-03-17 18:58:12 +0100

    Create HTMLBlockquote.swift

---
+++ Sources/HtmlUI/HTMLBlockquote.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct HTMLBlockquote: HTML {
+    var children: [HTML]
+    
+    init(@HTMLBuilder _ children: () -> [HTML]) {
+        self.children = children()
+    }
+    
+    func render() -> String {
+        return "<blockquote>\n" +
+        children.map { $0.render() }.joined(separator: "\n") +
+        "\n</blockquote>"
+    }
+}
