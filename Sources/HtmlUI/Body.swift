e51f374f2edf2030ec363fd6d6768a568c340df1
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-03-18 07:30:17 +0100

    Update Body.swift

--- Sources/HtmlUI/Body.swift
+++ Sources/HtmlUI/Body.swift
@@ -14,13 +14,13 @@ public struct Body: HTML {
         self.classStyles = classStyles
     }
     
-    func addClassName(_ className: String, @CSSBuilder _ properties: () -> [String]) -> Body {
+    public func addClassName(_ className: String, @CSSBuilder _ properties: () -> [String]) -> Body {
         var newClassStyles = self.classStyles
         newClassStyles[className] = properties()
         return Body(generalStyles: self.generalStyles, classStyles: newClassStyles)
     }
     
-    func render() -> String {
+    public func render() -> String {
         var styles = [String]()
         
         if !generalStyles.isEmpty {
