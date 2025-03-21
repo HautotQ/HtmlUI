1ecfae58886cf5c5e67de7e435c6f8428ada6cef
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-03-21 13:45:57 +0100

    Create HTMLSection.swift

---
+++ Sources/HtmlUI/HTMLSection.swift
@@ -0,0 +1,16 @@
+public struct HTMLSection: HTML {
+    var title: String?
+    var children: [HTML]
+    
+    public init(title: String? = nil, @StyleBuilder _ children: () -> [HTML]) {
+        self.title = title
+        self.children = children()
+    }
+    
+    public func render() -> String {
+        let titleHTML = title != nil ? "<h2>\(title!)</h2>\n" : ""
+        return "<section>\n" + titleHTML +
+        children.map { $0.render() }.joined(separator: "\n") +
+        "\n</section>"
+    }
+}
