f2afd994cc1950ab98397bb02084568e4a84ad37
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-03-21 07:50:57 +0100

    Update HTMLBuilder.swift

--- Sources/HtmlUI/HTMLBuilder.swift
+++ Sources/HtmlUI/HTMLBuilder.swift
@@ -2,26 +2,23 @@ import SwiftUI
 
 /// **HTMLBuilder** : Permet d'écrire plusieurs éléments HTML dans une closure
 @resultBuilder
-struct HTMLBuilder {
-    static func buildPartialBlock(first: any HTML) -> any HTML {
-        return first
-    }
-    static func buildPartialBlock(accumulated: any HTML, next: any HTML) -> any HTML {
-        return accumulated
+public struct HTMLBuilder {
+    public static func buildBlock(_ components: any HTML...) -> [any HTML] {
+        return components
     }
 }
 
 /// **CSSBuilder** : Permet d'écrire plusieurs styles CSS en closure
 @resultBuilder
-struct CSSBuilder {
-    static func buildBlock(_ components: String...) -> [String] {
+public struct CSSBuilder {
+    public static func buildBlock(_ components: String...) -> [String] {
         return components
     }
 }
 
 @resultBuilder
-struct HTMLBuilderArray {
-    static func buildBlock(_ components: any HTML...) -> [any HTML] {
+public struct HTMLBuilderArray {
+    public static func buildBlock(_ components: any HTML...) -> [any HTML] {
         return components
     }
 }
