560c521db8bb6f6c0bc8f287da3e797d61bb1bd2
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-03-24 07:52:00 +0100

    Update HTMLScript.swift

--- Sources/HtmlUI/HTMLScript.swift
+++ Sources/HtmlUI/HTMLScript.swift
@@ -1,14 +1,14 @@
 import SwiftUI
 
 /// Élément de texte HTML
-public struct HTMLScript: HTML {
-    let script: () -> String
+struct HTMLScript: HTML {
+    let script: String
     
-    public init(_ script: @escaping () -> String) {
-        self.script = script
+    init(_ script: @escaping () -> String) {
+        self.script = script()
     }
     
-    public func render() -> String {
-        return "<script>\(String(describing: script))</script>"
+    func render() -> String {
+        return "<script>\n\(script)\n</script>"
     }
 }
