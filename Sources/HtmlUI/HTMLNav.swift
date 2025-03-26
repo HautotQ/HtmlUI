b3a8289c4583880a3052d458fa61d3384cad3726
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-03-26 09:50:31 +0100

    Update HTMLNav.swift

--- Sources/HtmlUI/HTMLNav.swift
+++ Sources/HtmlUI/HTMLNav.swift
@@ -3,15 +3,18 @@ import SwiftUI
 public struct HTMLNav: HTML {
     var destination: HTMLDocument
     var label: [any HTML]
+    var onClick: String?
     
-    public init(destination: HTMLDocument, @HTMLBuilderArray content: () -> [any HTML]) {
+    public init(destination: HTMLDocument, onClick: String? = nil, @HTMLBuilderArray content: () -> [any HTML]) {
         self.destination = destination
+        self.onClick = onClick
         self.label = content()
     }
     
     public func render() -> String {
+        let onClickAttribute = onClick.map { " onclick=\"\($0)\"" } ?? ""
         return """
-        <a href="\(destination.title.lowercased()).html">
+        <a href="\(destination.title.lowercased()).html"\(onClickAttribute)>
             \(label.map { $0.render() }.joined(separator: "\n"))
         </a>
         """
