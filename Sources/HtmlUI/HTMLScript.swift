57dca958ff9dc0ea42d5a3e31a911c3b7aa97b1b
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-04-08 14:01:39 +0200

    Update HTMLScript.swift

--- Sources/HtmlUI/HTMLScript.swift
+++ Sources/HtmlUI/HTMLScript.swift
@@ -1,14 +1,93 @@
 import SwiftUI
 
-/// Élément de texte HTML
+/// Élément <script> HTML compatible HTML5 avec tous les attributs possibles
 public struct HTMLScript: HTML {
-    let script: String
-    
-    public init(_ script: @escaping () -> String) {
+    private let script: String?
+    private let src: String?
+    private let type: String?
+    private let async: Bool
+    private let deferScript: Bool
+    private let nomodule: Bool
+    private let crossorigin: String?
+    private let referrerpolicy: String?
+    private let integrity: String?
+
+    /// Script inline
+    public init(
+        type: String? = nil,
+        async: Bool = false,
+        defer: Bool = false,
+        nomodule: Bool = false,
+        crossorigin: String? = nil,
+        referrerpolicy: String? = nil,
+        integrity: String? = nil,
+        _ script: @escaping () -> String
+    ) {
         self.script = script()
+        self.src = nil
+        self.type = type
+        self.async = async
+        self.deferScript = `defer`
+        self.nomodule = nomodule
+        self.crossorigin = crossorigin
+        self.referrerpolicy = referrerpolicy
+        self.integrity = integrity
     }
-    
+
+    /// Script externe
+    public init(
+        src: String,
+        type: String? = nil,
+        async: Bool = false,
+        defer: Bool = false,
+        nomodule: Bool = false,
+        crossorigin: String? = nil,
+        referrerpolicy: String? = nil,
+        integrity: String? = nil
+    ) {
+        self.script = nil
+        self.src = src
+        self.type = type
+        self.async = async
+        self.deferScript = `defer`
+        self.nomodule = nomodule
+        self.crossorigin = crossorigin
+        self.referrerpolicy = referrerpolicy
+        self.integrity = integrity
+    }
+
     public func render() -> String {
-        return "<script>\n\(script)\n</script>"
+        var attributes: [String] = []
+
+        if let src = src {
+            attributes.append("src=\"\(src)\"")
+        }
+        if let type = type {
+            attributes.append("type=\"\(type)\"")
+        }
+        if async {
+            attributes.append("async")
+        }
+        if deferScript {
+            attributes.append("defer")
+        }
+        if nomodule {
+            attributes.append("nomodule")
+        }
+        if let crossorigin = crossorigin {
+            attributes.append("crossorigin=\"\(crossorigin)\"")
+        }
+        if let referrerpolicy = referrerpolicy {
+            attributes.append("referrerpolicy=\"\(referrerpolicy)\"")
+        }
+        if let integrity = integrity {
+            attributes.append("integrity=\"\(integrity)\"")
+        }
+
+        if let script = script {
+            return "<script \(attributes.joined(separator: " "))>\n\(script)\n</script>"
+        } else {
+            return "<script \(attributes.joined(separator: " "))></script>"
+        }
     }
 }
